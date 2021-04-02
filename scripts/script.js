a28b8ad830d91b08d73020030a335106ec8040f1
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2021-04-02 00:14:58 -0300

    Fim da implementação web, faltando apenas fazer a parte de link com whatsapp

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ let drinkName = "";
 let drinkPrice = 0;
 let dessertName = "";
 let dessertPrice = 0;
+let totalPrice = 0;
 let clientName = "";
 let clientAdress = "";
 let choicesSelected = 0;
@@ -16,11 +17,11 @@ function selectMain(main){
     choicesSelected--;
   }
   
-  let seletor="."+main+" .selection";
+  let seletor="." + main + " .selection";
   const c = document.querySelector(seletor);
   c.classList.add("display");
   choicesSelected++;
-  mainName = document.querySelector("."+main+" h2").innerHTML;;
+  mainName = document.querySelector("." + main + " h2").innerHTML;
   mainPrice = parseFloat(document.querySelector("."+main+" span").innerHTML.replace(",","."));
 
  
@@ -43,12 +44,12 @@ function selectDrink(drink){
     choicesSelected--;
   }
 
-  let seletor="."+drink+" .selection";
+  let seletor="." + drink + " .selection";
   const c = document.querySelector(seletor);
   c.classList.add("display");
   choicesSelected++;
 
-  drinkName = document.querySelector("."+drink+" h2").innerHTML;
+  drinkName = document.querySelector("." + drink + " h2").innerHTML;
   drinkPrice = parseFloat(document.querySelector("."+drink+" span").innerHTML.replace(",","."));
 
   console.log(mainName)
@@ -68,13 +69,13 @@ function selectDessert(dessert){
     choicesSelected--;
   }
 
-  let seletor="."+dessert+" .selection";
+  let seletor = "." + dessert + " .selection";
   const c = document.querySelector(seletor);
   c.classList.add("display");
   choicesSelected++;
 
-  dessertName = document.querySelector("."+dessert+" h2").innerHTML;;
-  dessertPrice = parseFloat(document.querySelector("."+dessert+" span").innerHTML.replace(",","."));
+  dessertName = document.querySelector("." + dessert + " h2").innerHTML;
+  dessertPrice = parseFloat(document.querySelector("." + dessert + " span").innerHTML.replace(",","."));
   
   console.log(mainName)
   console.log(mainPrice)
@@ -95,5 +96,17 @@ function check(){
 }
 
 function finalScreen(){
+  totalPrice = mainPrice + drinkPrice + dessertPrice;
+  document.querySelector(".main-name").innerHTML = mainName;
+  document.querySelector(".main-price").innerHTML = String(mainPrice.toFixed(2).replace(".",","));
+  document.querySelector(".drink-name").innerHTML = drinkName;
+  document.querySelector(".drink-price").innerHTML = String(drinkPrice.toFixed(2).replace(".",","));
+  document.querySelector(".dessert-name").innerHTML = dessertName;
+  document.querySelector(".dessert-price").innerHTML = String(dessertPrice.toFixed(2).replace(".",","));
+  document.querySelector(".total-price").innerHTML = String(totalPrice.toFixed(2).replace(".",","));
   document.querySelector(".confirmation-background").classList.add("display");
+}
+
+function cancel(){
+  document.querySelector(".confirmation-background").classList.remove("display");
 }
