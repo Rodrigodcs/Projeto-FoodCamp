88197b8da746dd71cb2df1ee6d7738d40d0a3e24
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2021-04-01 21:15:56 -0300

    Finalização do HTML/CSS e inicio da implementação do funcionalidade de selecionar

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,16 @@
-function compra(){
-  var x = document.getElementById("teste").innerHTML
+let mainPrice = 0;
+let drinkPrice = 0;
+let dessertPrice = 0;
+let done = 0;
+
+function select(food){
+
+  if(document.querySelector(".display")!==null){
+    document.querySelector(".display").classList.remove("display")
+  }
+  let seletor="."+food+" .selection";
+  console.log(seletor);
+  const c = document.querySelector(seletor);
+  c.classList.add("display");
   
-  alert(parseInt(x)+2);
 }
